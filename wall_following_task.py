bbca6c91d66a3caaf8ac2e93ce6d6f255e51d510
xloc <tj-xzx@126.com>
2017-06-03 05:08:32 +0800

    Improve parameters

--- wall_following_task.py
+++ wall_following_task.py
@@ -64,17 +64,17 @@ class PlanterWallFollower(t.Task):
         distance_front_left = t.p.ranging.get(4)
         distance_rear_left = t.p.ranging.get(3)
 
-        lower_boundary = 150
-        upper_boundary = 220
+        lower_boundary = 180
+        upper_boundary = 280
         error_distance = 10
 
         print '\033[1;36m{}\033[0m'.format('WALL FOLLOWING START')
-        if 20 < distance_front < 400:
+        if 20 < distance_front < 300:
             l, r = [0, 0]
             self.done = True
         else:
-            if distance_front_left > 1000 or distance_rear_left > 1000:
-                l, r = [0.5, 0.5]
+            if distance_front_left > 5000 or distance_rear_left > 5000:
+                l, r = [0, 0]
             else:
                 if distance_front_left == 0 and 10 < distance_rear_left < upper_boundary:
                     l, r = [0.2, 0.9]
@@ -88,24 +88,63 @@ class PlanterWallFollower(t.Task):
                             l, r = [0.5, 0.2]
                         # case 2
                         elif distance_front_left > upper_boundary and distance_rear_left > upper_boundary:
-                            l, r = [0.1, 0.6]
+                            if distance_front_left > 900 and distance_rear_left > 900 and distance_front > 500:
+                                l, r = [0.1, 0.9]
+
+                                self.step_manager.add_n_times(
+                                    setlr_closure(0.1, 0.9),
+                                    times=duration2times(0.7)
+                                )
+                                self.step_manager.add_n_times(
+                                    setlr_closure(0.3, 0.3),
+                                    times=duration2times(0.4)
+                                )
+
+                            elif 500 < distance_front_left <= 900 or 500 < distance_rear_left <= 900:
+                                self.step_manager.add_n_times(
+                                    setlr_closure(0.2, 0.5),
+                                    times=duration2times(0.2)
+                                )
+                                # st1 = time.time()
+                                # while time.time() < st1 + 0.2:
+                                #     l, r = [0.5, 0.2]
+                                #     t.peripheral.wheels.set_lr(r, l)
+                            else:
+                                if (distance_rear_left - distance_front_left) < 20:
+                                    self.step_manager.add_n_times(
+                                        setlr_closure(0.2, 0.5),
+                                        times=duration2times(0.2)
+                                    )
+                                    # st1 = time.time()
+                                    # while time.time() < st1 + 0.2:
+                                    #     l, r = [0.5, 0.1]
+                                    #     t.peripheral.wheels.set_lr(r, l)
+                                else:
+                                    self.step_manager.add_n_times(
+                                        setlr_closure(0.5, 0.2),
+                                        times=duration2times(0.2)
+                                    )
+
                         # case 3
                         elif distance_front_left < lower_boundary and distance_rear_left > lower_boundary:
-                            l, r =[0.5, 0.1]
+                            l, r =[0.5, 0.2]
                         # case 4
                         elif distance_front_left > lower_boundary and distance_rear_left < lower_boundary:
-                            self.step_manager.add_n_times(
-                                setlr_closure(0.4, 0.4),
-                                times=duration2times(0.3)
-                            )
-                            l, r = [0.1, 0.5]
+                            if distance_front_left > 500 or distance_rear_left < 10:
+                                l, r = [0.1, 0.5]
+                            else:
+                                self.step_manager.add_n_times(
+                                    setlr_closure(0.4, 0.4),
+                                    times=duration2times(0.3)
+                                )
+                                l, r = [0.1, 0.5]
                         # case 5
                         elif distance_front_left < upper_boundary and distance_rear_left > upper_boundary:
                             self.step_manager.add_n_times(
                                 setlr_closure(0.4, 0.4),
                                 times=duration2times(0.3)
                             )
-                            l, r = [0.5, 0.1]
+                            l, r = [0.5, 0.2]
                         # case 6
                         elif distance_front_left > upper_boundary and distance_rear_left < upper_boundary:
                             l, r = [0.1, 0.6]
